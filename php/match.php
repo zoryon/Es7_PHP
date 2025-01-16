b6b8c5b9f2b740e8f954117b3e497a4d0d472482
zoryon <gioelespata@gmail.com>
2025-01-16 18:12:17 +0100

    fixing

--- php/match.php
+++ php/match.php
@@ -5,7 +5,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     
     <!-- style links -->
-    <link rel="stylesheet" href="./css/globals.css">
+    <link rel="stylesheet" href="../css/globals.css">
 
     <!-- settings -->
     <meta charset="UTF-8">
@@ -20,88 +20,106 @@
     <?php
         session_start(); 
 
+        // validating
         // if no username was set in the POST & in the SESSION, missing data should be detected
-        if (!isset($_POST["username"])) {
-            if (!isset($_SESSION["username"])) {
-                header("Location: ./error.html");
-                session_destroy();
-                exit();
-            }
+        if (!isset($_POST["username"]) && !isset($_SESSION["username"])) {
+            header("Location: ./error.html");
+            session_destroy();
+            exit("No username was set.");
+        }
+
+        // if the set username equals to "" & it is not saved in the SESSION, missing data should be detected
+        if (empty($_POST["username"]) && !isset($_SESSION["username"])) {
+            header("Location: ./error.html");
+            session_destroy();
+            exit("No username was set.");
         }
 
         // if no username was set in the SESSION, the game has yet to start => set starting variables
         if (!isset($_SESSION["username"])) {
-            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["username"] = $_POST["username"]; // the POST has username, otherwise the other if would have been executed
             $_SESSION["userWins"] = 0;
             $_SESSION["computerWins"] = 0;
             $_SESSION["turn"] = 1;
         }
 
+        // game logic
         // if a choice was made
         if (isset($_POST["choice"])) {
-            $userChoice = $_POST["choice"];
+            $userChoice = (int)$_POST["choice"]; // cast would not be needed, but it is safer
             $computerChoice = rand(1, 9);
             
-            if ($userChoice == $computerChoice || $userChoice == $computerChoice + 1) {
+            if ($userChoice === $computerChoice || $userChoice === $computerChoice + 1) {
                 $_SESSION["userWins"]++;
             } else {
                 $_SESSION["computerWins"]++;
             }
             
-            $_SESSION["turn"]++;
+            if (!hasMatchEnded()) {
+                $_SESSION["turn"]++;
+            }
 
             // update choices
             $_SESSION["userLastChoice"] = $userChoice;
             $_SESSION["computerLastChoice"] = $computerChoice;
         }
 
-        // the one to win 3 matches wins
-        if ($_SESSION["userWins"] >= 3) {
-            echo "<p>Congratulations, " . $_SESSION["username"] . "! You won the game.</p>";
+        $userWon = hasWon("userWins");
+        $computerWon = hasWon("computerWins");
+
+        if ($userWon || $computerWon) {
             session_destroy();
-            exit();
-        } elseif ($_SESSION["computerWins"] >= 3) {
-            echo "<p>You lost the game, " . $_SESSION["username"];
-            session_destroy();
-            exit();
+        }
+
+        function hasWon($player) {
+            if ($player != "userWins" && $player != "computerWins") return null;
+            
+            return $_SESSION[$player] >= 3;
+        }
+
+        function hasMatchEnded() {
+            return hasWon("userWins") || hasWon("computerWins");
         }
     ?>
 
-    <main class="w-full flex flex-col items-center justify-center gap-6 pt-2">
+    <main>
         <h1 class="text-3xl font-bold">Higher number game</h1>
 
         <p>Username: <?= $_SESSION["username"] ?></p>
         <p>Turn: <?= $_SESSION["turn"] ?></p>
 
         <div class="flex flex-col justify-center items-center">
-            <?php if (isset($_SESSION["userLastChoice"])) echo "<p>User's last choice: " . $_SESSION["userLastChoice"] . "</p>" ?>
-            <?php if (isset($_SESSION["computerLastChoice"])) echo "<p>Computer's last choice: " . $_SESSION["computerLastChoice"] . "</p>" ?>
+            <p><?= isset($_SESSION["userLastChoice"]) ? "User's last choice: " . $_SESSION["userLastChoice"] : "" ?></p>
+            <p><?= isset($_SESSION["computerLastChoice"]) ? "Computer's last choice: " . $_SESSION["computerLastChoice"] : "" ?></p>
+
         </div>
 
-        <div class="flex flex-col justify-center items-center">
-            <?php if (isset($_SESSION["userWins"])) echo "<p>User Wins: " . $_SESSION["userWins"] . "</p>" ?>
-            <?php if (isset($_SESSION["computerWins"])) echo "<p>Computer Wins: " . $_SESSION["computerWins"] . "</p>" ?>
+        <div class="flex flex-col justify-center items-center font-bold">
+            <p>User's wins: <?= $_SESSION["userWins"] ?></p>
+            <p>Computer's wins: <?= $_SESSION["computerWins"] ?></p>
         </div>
 
-        <form action="./match.php" method="POST" class="flex gap-2 items-center">
-            <label for="choice">Choose your number </label>
-            <select id="choice" name="choice" class="border border-1 rounded-md">
-                <option value="1" selected="selected">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-            </select>
+        <div class="flex flex-col justify-center items-center font-bold underline">
+            <p><?= $userWon ? "Congratulations, " . $_SESSION["username"] . "! You won the game." : "" ?></p>
+            <p><?= $computerWon ? "You lost the game!" : "" ?></p>
+        </div>
+
+        <form action="<?= hasMatchEnded() ? "../index.html" : "./match.php" ?>" method="POST" class="flex flex-col gap-2 justify-center items-center">
+            <div>
+                <label for="choice">Choose your number </label>
+
+                <select id="choice" name="choice" class="border border-1 rounded-md">
+                    <?php for ($i = 1; $i <= 9; $i++): ?>
+                        <option value="<?= $i ?>"><?= $i ?></option>
+                    <?php endfor; ?>
+                </select>
+            </div>
 
             <button 
                 type="submit" 
                 class="bg-blue-300 text-sm border border-1 px-4 py-1 rounded-md"
             >
-                Play
+                <?= hasMatchEnded() ? "Another match" : "Play" ?>
             </button>
         </form>
     </main>
